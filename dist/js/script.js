d4801df3520bdfbb1bfa02a630dc7b3c8890512f
yunusf <yunusf42@gmail.com>
2023-10-28 09:39:14 +0700

    menambahkan prettier, smoth scroll, tombol balik ke beranda, dark mode, mengecilkan size css

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
     const header = document.querySelector('header') // mengambil elemen header
     const fixedNav = header.offsetTop
+    const top = document.querySelector('#top')
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed')
+        top.classList.remove('hidden')
+        top.classList.add('flex')
     } else {
         header.classList.remove('navbar-fixed')
+        top.classList.add('hidden')
+        top.classList.remove('flex')
     }
 }
 
@@ -14,7 +19,48 @@ window.onscroll = function () {
 const hamburger = document.querySelector('#hamburger') // mengambil id hamburger
 const navMenu = document.querySelector('#nav-menu') // mengambil id
 
+
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden')
 })
+
+// klik di luar Humberger
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden')
+    }
+})
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle') // checkbox
+const html = document.querySelector('html')
+
+const lRounded = document.querySelector('#long-rounded')
+const rounded = document.querySelector('#rounded')
+
+darkToggle.addEventListener('click', function () {
+    lRounded.classList.toggle('bg-slate-700')
+    lRounded.classList.toggle('bg-white')
+
+    rounded.classList.toggle('bg-white')
+    rounded.classList.toggle('bg-slate-700')
+
+    // darkToggle.checked ? html.classList.add('dark') : html.classList.remove('dark')
+    if (darkToggle.checked) {
+        html.classList.add('dark')
+        localStorage.theme = 'dark' // menambahkan tema dark di local storage
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light' // menambahkan tema light di local storage
+    }
+})
+
+// memindahkan posisi toggle sesuai momde
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true
+} else {
+    darkToggle.checked = false
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -9,7 +10,9 @@ module.exports = {
     extend: {
       colors: {
         primary: '#a855f7', // dapat diakses dengan mudah di class
-        dark: '#020617',
+        // dark: '#020617', slate-950
+        // dark: '#172554', blue-950
+        dark: '#0f172a', // slate-900
         secondary: '#64748b'
       },
       screens: {
